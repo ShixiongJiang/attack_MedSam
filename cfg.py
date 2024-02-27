65c587043b1b84b3c21dda788b87f7a47df9e050
ShixiongJiang <jiangshixiong0330@gmail.com>
2024-02-26 22:32:21 -0500

    atta_function.py

--- cfg.py
+++ cfg.py
@@ -45,7 +45,7 @@ def parse_args():
     parser.add_argument('-evl_chunk', type=int, default=None , help='evaluation chunk')
     parser.add_argument('-mid_dim', type=int, default=None , help='middle dim of adapter or the rank of lora matrix')
     parser.add_argument('-epsilon', type=int, default=0.05 , help='define the power of attack')
-    parser.add_argument('-attack_method', type=int, default='pgd' , help='define the power of attack')
+    parser.add_argument('-attack_method', type=str, default='pgd' , help='define the power of attack')
 
 
     # parser.add_argument(
