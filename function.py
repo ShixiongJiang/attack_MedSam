ba29c4d7e2475db9766ea3bc5c85545f7e5af1a0
ShixiongJiang <jiangshixiong0330@gmail.com>
2024-05-30 13:30:17 -0400

    atta_function.py

--- function.py
+++ function.py
@@ -954,6 +954,7 @@ def heat_map( args, net, train_loader, lossfunc):
                 def backward_hook(module, grad_in, grad_out):
                     nonlocal  encoder_grad
                     encoder_grad = grad_out[0]
+                    print(grad_out)
                 net.image_encoder.register_forward_hook(encoder_hook)
                 net.image_encoder.register_backward_hook(backward_hook)
 
