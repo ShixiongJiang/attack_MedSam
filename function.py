9aee4a4fefe0f2cd22f95de28f9c57259f400b0c
ShixiongJiang <jiangshixiong0330@gmail.com>
2024-06-12 00:25:18 -0400

    feature map

--- function.py
+++ function.py
@@ -996,24 +996,22 @@ def heat_map(args, net, train_loader, lossfunc):
                 targets = [SemanticSegmentationTarget(masks)]
                 # targets = None
 
-                with torch.no_grad():
-                    imgs = imgs.requires_grad_(True)
-                    cam = GradCAM(model=net,
-                                  target_layers=target_layers)
-                    grayscale_cam = cam(input_tensor=imgs, targets=targets)
+                cam = GradCAM(model=net,
+                              target_layers=target_layers)
+                grayscale_cam = cam(input_tensor=imgs, targets=targets)
 
 
-                    # print(grayscale_cam)
-                    # grayscale_cam = grayscale_cam.cpu().numpy()
-                    #
-                    # # Convert the input image to a numpy array and transpose it to (H, W, C) format
-                    # input_image_np = imgs.squeeze().permute(1, 2, 0).cpu().numpy()
-                    # input_image_np = (input_image_np - input_image_np.min()) / (
-                    #             input_image_np.max() - input_image_np.min())
+                # print(grayscale_cam)
+                # grayscale_cam = grayscale_cam.cpu().numpy()
+                #
+                # # Convert the input image to a numpy array and transpose it to (H, W, C) format
+                # input_image_np = imgs.squeeze().permute(1, 2, 0).cpu().numpy()
+                # input_image_np = (input_image_np - input_image_np.min()) / (
+                #             input_image_np.max() - input_image_np.min())
 
-                    # print(cam_image.shape)
-                    # Image.fromarray(cam_image)
+                # print(cam_image.shape)
+                # Image.fromarray(cam_image)
 
-                    break
+                break
 
     # torch.softmax(pred)
