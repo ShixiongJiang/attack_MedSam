d78af19cf998ba7e613f0d319a136aeeeacf9a35
sjian <jiangshixiong0330@gmail,com>
2024-06-27 23:06:37 +0800

    commit

--- function.py
+++ function.py
@@ -966,7 +966,7 @@ def heat_map(args, net, train_loader, lossfunc):
 
                 # print(net)
                 # backward_hook = net.mask_decoder.iou_prediction_head.register_full_backward_hook(backward_hook, prepend=False)
-                backward_hook = net.mask_decoder.iou_prediction_head.register_backward_hook(backward_hook, prepend=False)
+                backward_hook = net.mask_decoder.iou_prediction_head.register_backward_hook(backward_hook)
 
                 forward_hook = net.mask_decoder.iou_prediction_head.register_forward_hook(forward_hook, prepend=False)
 
