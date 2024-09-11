c2ac6cf7c1672ddacb3c7091ffc677d782317e25
sjian <jiangshixiong0330@gmail,com>
2024-09-11 13:49:46 -0400

    commit

--- train_reverse_adaptation.py
+++ train_reverse_adaptation.py
@@ -22,9 +22,30 @@ args = cfg_reverse_adaptation.parse_args()
 GPUdevice = torch.device('cuda', args.gpu_device)
 
 net = get_network(args, args.net, use_gpu=args.gpu, gpu_device=GPUdevice, distribution = args.distributed)
-if args.pretrain:
-    weights = torch.load(args.pretrain)
-    net.load_state_dict(weights,strict=False)
+'''load pretrained model'''
+assert args.weights != 0
+print(f'=> resuming from {args.weights}')
+assert os.path.exists(args.weights)
+checkpoint_file = os.path.join(args.weights)
+assert os.path.exists(checkpoint_file)
+loc = 'cuda:{}'.format(args.gpu_device)
+checkpoint = torch.load(checkpoint_file, map_location=loc)
+start_epoch = checkpoint['epoch']
+best_tol = checkpoint['best_tol']
+
+state_dict = checkpoint['state_dict']
+if args.distributed != 'none':
+    from collections import OrderedDict
+    new_state_dict = OrderedDict()
+    for k, v in state_dict.items():
+        # name = k[7:] # remove `module.`
+        name = 'module.' + k
+        new_state_dict[name] = v
+    # load params
+else:
+    new_state_dict = state_dict
+
+net.load_state_dict(new_state_dict)
 
 optimizer = optim.Adam(net.parameters(), lr=args.lr, betas=(0.9, 0.999), eps=1e-08, weight_decay=0, amsgrad=False)
 scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.5) #learning rate decay
@@ -63,8 +84,8 @@ transform_test_seg = transforms.Compose([
 
 
 print('if poison ', args.poison)
-'''polyp data'''
-polyp_train_dataset = Polyp(args, args.data_path, transform = transform_train, transform_msk= transform_train_seg, mode = 'Training')
+'''polyp data using the original dataset to generate poison sample'''
+polyp_train_dataset = Polyp_generated_poison_lora(args, args.data_path, transform = transform_train, transform_msk= transform_train_seg, mode = 'Training')
 polyp_test_dataset = Polyp(args, args.data_path, transform = transform_test, transform_msk= transform_test_seg, mode = 'Test')
 
 nice_train_loader = DataLoader(polyp_train_dataset, batch_size=args.b, shuffle=True, num_workers=0, pin_memory=True)
@@ -92,43 +113,18 @@ checkpoint_path = os.path.join(checkpoint_path, '{net}-{epoch}-{type}.pth')
 '''begin training'''
 best_acc = 0.0
 best_tol = 1e4
+total_epoch = 10
 
-
-for epoch in range(settings.EPOCH):
-    # if epoch and epoch < 5:
-    #     tol, eiou, edice = function.validation_sam(args, nice_test_loader, epoch, net, writer)
-    #     logger.info(f'Total score: {tol}, IOU: {eiou}, DICE: {edice} || @ epoch {epoch}.')
+# for epoch in range(settings.EPOCH):
+for epoch in range(total_epoch):
 
     net.train()
-    time_start = time.time()
-    loss = function.train_sam(args, net, optimizer, nice_train_loader, epoch, writer, vis = args.vis)
-    logger.info(f'Train loss: {loss} || @ epoch {epoch}.')
-    time_end = time.time()
-    print('time_for_training ', time_end - time_start)
+    function.train_sam(args, net, optimizer, nice_train_loader, epoch, writer)
 
-    net.eval()
-    if epoch and epoch % args.val_freq == 0 or epoch == settings.EPOCH-1:
+    if epoch and epoch % args.val_freq == 0 or epoch == total_epoch-1:
         tol, eiou, edice = function.validation_sam(args, nice_test_loader, epoch, net, writer)
         logger.info(f'Total score: {tol}, IOU: {eiou}, DICE: {edice} || @ epoch {epoch}.')
 
-        if args.distributed != 'none':
-            sd = net.module.state_dict()
-        else:
-            sd = net.state_dict()
 
-        if tol < best_tol:
-            best_tol = tol
-            is_best = True
-
-            save_checkpoint({
-            'epoch': epoch + 1,
-            'model': args.net,
-            'state_dict': sd,
-            'optimizer': optimizer.state_dict(),
-            'best_tol': best_tol,
-            'path_helper': args.path_helper,
-        }, is_best, args.path_helper['ckpt_path'], filename="best_checkpoint")
-        else:
-            is_best = False
 
 writer.close()
