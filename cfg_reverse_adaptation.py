ef8e9eb38a87ff4842ee9e3fe21907cb1dd3a302
sjian <jiangshixiong0330@gmail,com>
2024-10-25 12:45:42 -0500

    commit

--- cfg_reverse_adaptation.py
+++ cfg_reverse_adaptation.py
@@ -55,6 +55,7 @@ def parse_args():
     parser.add_argument('-epsilon', type=float, default=0.1 , help='define the power of attack')
     parser.add_argument('-attack_method', type=str, default='pgd' , help='define the power of attack')
     parser.add_argument('-freeze', type=bool, default=True, help='define the power of attack')
+    parser.add_argument('--num_processes', type=int, default=1, help='Total number of processes')
 
     parser.add_argument("--backdoor", type=bool, default=False, help="indicate backdoor attack")
     # parser.add_argument("--poison_datasets", type=str,nargs="+",default="poison_dataset")
