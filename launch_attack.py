f6704b834e39932a56e5e9c6cd126c9c27958ecd
Jialiang Fan <jfan5@nd.edu>
2024-10-26 12:06:29 -0400

    update

--- launch_attack.py
+++ launch_attack.py
@@ -1,6 +1,23 @@
-import subprocess
 import argparse
 import os
+import torch.multiprocessing as mp
+
+def run_attack(process_idx, num_processes, script, additional_args):
+    # 设置每个进程的 GPU
+    os.environ['CUDA_VISIBLE_DEVICES'] = str(process_idx)
+    print(f"Launching process {process_idx} on GPU {process_idx}")
+
+    cmd = [
+        'python', script,
+        '--process_idx', str(process_idx),
+        '--num_processes', str(num_processes),
+        '--gpu_device', str(process_idx)
+    ]
+    if additional_args:
+        cmd.extend(additional_args.strip().split())
+
+    # 使用 os.system 来运行命令
+    os.system(" ".join(cmd))
 
 def main():
     parser = argparse.ArgumentParser(description='Launch Multiple Processes for One Pixel Attack')
@@ -9,28 +26,17 @@ def main():
     parser.add_argument('--additional_args', type=str, default='', help='Additional arguments to pass to the script')
     args = parser.parse_args()
 
-    num_processes = args.num_processes
+    # 启动多进程
     processes = []
-
-    for i in range(num_processes):
-        env = os.environ.copy()
-        env['CUDA_VISIBLE_DEVICES'] = str(i)
-
-        cmd = [
-            'python', args.script,
-            '--process_idx', str(i),
-            '--num_processes', str(num_processes),
-            '--gpu_device', str(i),  # 修改为 i，确保每个进程使用不同的 GPU
-        ]
-        if args.additional_args:
-            cmd.extend(args.additional_args.strip().split())
-
-        print(f"Launching process {i} on GPU {i}")
-        p = subprocess.Popen(cmd, env=env)
+    for i in range(args.num_processes):
+        p = mp.Process(target=run_attack, args=(i, args.num_processes, args.script, args.additional_args))
+        p.start()
         processes.append(p)
 
+    # 等待所有进程完成
     for p in processes:
-        p.wait()
+        p.join()
 
 if __name__ == '__main__':
+    mp.set_start_method('spawn')  # 使用 'spawn' 方法启动进程以确保多进程的兼容性
     main()
