043e99be7cb88232f003dee9f5f71e2a902d62be
Jialiang Fan <jfan5@nd.edu>
2024-10-26 12:30:04 -0400

    update

--- utils.py
+++ utils.py
@@ -369,7 +369,7 @@ def set_log_dir(root_dir, exp_name):
     now = datetime.now(dateutil.tz.tzlocal())
     timestamp = now.strftime('%Y_%m_%d_%H_%M_%S')
     prefix = exp_path + '_' + timestamp
-    os.makedirs(prefix)
+    os.makedirs(prefix,exist_ok=True)
     path_dict['prefix'] = prefix
 
     # set checkpoint path
